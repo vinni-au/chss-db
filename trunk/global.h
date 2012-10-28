d4a7b639b55bcd3588952c8e997ae910858026f3
antonstorozhev@gmail.com <antonstorozhev@gmail.com@58bde72c-ba86-4c16-7f69-a417a3a209a5>
2012-10-28 14:43:47 +0000

    Created component's subdir's

---
+++ trunk/global.h
@@ -0,0 +1,8 @@
+#ifndef _GLOBAL_H_
+#define _GLOBAL_H_
+/*
+ * Global declarations for chss-db
+ */
+
+
+#endif //_GLOBAL_H_
---
+++ trunk/main.cpp
@@ -0,0 +1,11 @@
+/*
+ * Chss-db main program
+ *
+ */
+ 
+#include <iostream> 
+ 
+int main(int argc, char* argv[])
+{
+    return 0;
+}
