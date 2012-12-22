e45f1f3644d0ea8c6d171b2b0bbdc7498a99c24e
antonstorozhev@gmail.com <antonstorozhev@gmail.com@58bde72c-ba86-4c16-7f69-a417a3a209a5>
2012-12-22 17:00:07 +0000

    CreateQuery

--- trunk/query/query.hpp
+++ trunk/query/query.hpp
@@ -1,15 +1,35 @@
 #ifndef QUERY_HPP
 #define QUERY_HPP
 
-#include <string>
+#include "../global.h"
+
+enum QueryType {
+    Q_Query,
+    Q_Create,
+    Q_Select
+};
 
 struct Query
 {
-    Query();
+    Query() : m_type(Q_Query)
+    {}
 
     static Query* parse(std::string query);
 
-private:
+protected:
+    QueryType m_type;
+};
+
+struct CreateQuery : Query
+{
+    CreateQuery()
+    {
+        m_type = Q_Create;
+    }
+
+protected:
+    std::string m_tablename;
+    std::vector<std::string, int> m_columns;
 };
 
 #endif // QUERY_HPP
