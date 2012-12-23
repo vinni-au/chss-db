b6b857f3e641a744a1caf71d25519652a7883f49
evgentu@gmail.com <evgentu@gmail.com@58bde72c-ba86-4c16-7f69-a417a3a209a5>
2012-12-23 20:51:45 +0000

    Покушать принёс

--- trunk/global.cpp
+++ trunk/global.cpp
@@ -1,6 +1,12 @@
 #include "global.h"
+#include <sstream>
 
 inline void postError(const char *who, const char *message) {
     std::cerr << "ERROR in " << who << ": " << message << std::endl;
 }
 
+std::string int_to_string(int value) {
+    std::stringstream ss;
+    ss << value;
+    return ss.str();
+}
