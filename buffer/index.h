bd3b4568dc644a3cd4006277ec5a86d5b09f461f
evgentu@gmail.com <evgentu@gmail.com@58bde72c-ba86-4c16-7f69-a417a3a209a5>
2013-01-27 17:26:31 +0000

    Noindex file

--- buffer/index.h
+++ buffer/index.h
@@ -3,6 +3,7 @@
 
 #include "record.h"
 #include "buffermanager.h"
+#include "indexfile.h"
 
 struct Index;
 
@@ -10,19 +11,20 @@ struct IndexIterator {
     IndexIterator(Index* index, int key) : m_index(index), m_key(key) {}
     virtual Record* getNextRecord() = 0;
     virtual bool hasNextRecord() = 0;
-private:
+protected:
     Index* m_index;
     int m_key;
 };
 
 struct Index {
-    Index(BufferManager* bm, Signature* signature, uint32 table_id, uint32 column) :
-        m_bm(bm), m_signature(signature), m_table_id(table_id), m_column(column) {}
+    Index(IndexFile* file, BufferManager* bm, Signature* signature, uint32 table_id, uint32 column) :
+        m_file(file), m_bm(bm), m_signature(signature), m_table_id(table_id), m_column(column) {}
     virtual void createIndex() = 0;
     virtual void addKey(int key, uint32 page) = 0;
     virtual IndexIterator* findKey(int key) = 0;
     virtual void deleteKey(int key, uint32 page) = 0;
-private:
+
+    IndexFile* m_file;
     BufferManager* m_bm;
     Signature* m_signature;
     uint32 m_table_id;
--- buffer/noindex.cpp
+++ buffer/noindex.cpp
@@ -5,11 +5,25 @@ NoindexIterator::NoindexIterator(Index* index, int key) : IndexIterator(index, k
 }
 
 Record* NoindexIterator::getNextRecord() {
-
+    if(hasNextRecord()) {
+        return current_record;
+    }
+    return 0;
 }
 
 bool NoindexIterator::hasNextRecord() {
-
+    if(!current_record) {
+        uint32 size = m_index->m_file->get_size();
+        while(m_current_position < size) {
+            ++m_current_position;
+            current_record = m_index->m_file->get(m_current_position);
+            if(current_record->getInt(m_index->m_column) == m_key)
+                break;
+            delete current_record;
+            current_record = 0;
+        }
+    }
+    return current_record != 0;
 }
 
 void Noindex::createIndex() {
@@ -19,8 +33,8 @@ void Noindex::addKey(int key, uint32 page) {
 }
 
 NoindexIterator* Noindex::findKey(int key) {
+    return new NoindexIterator(this, key);
 }
 
 void Noindex::deleteKey(int key, uint32 page) {
-
 }
